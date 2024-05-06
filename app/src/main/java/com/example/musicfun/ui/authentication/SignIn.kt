27a2689146d5448d96c8acba5ae583e33bc27c7f
jcy <1055655665@qq.com>
2024-05-06 21:30:13 +0800

    merge Old MainActivity To HomeActivity

--- app/src/main/java/com/example/musicfun/ui/authentication/SignIn.kt
+++ app/src/main/java/com/example/musicfun/ui/authentication/SignIn.kt
@@ -15,6 +15,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.musicfun.R
+import com.example.musicfun.HomeActivity
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +40,11 @@ class SignIn : AppCompatActivity() {
             insets
         }
 
+        val btnSignIn = findViewById<Button>(R.id.btn_sign_in)
+        btnSignIn.setOnClickListener {
+            val intent = Intent(this@SignIn, HomeActivity::class.java)
+            startActivity(intent)
+        }
         val btnSignUp = findViewById<Button>(R.id.btn_sign_up)
         btnSignUp.setOnClickListener {
             val intent = Intent(this@SignIn, SignUp::class.java)
